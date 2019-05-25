cb8066a0922820bea7fd070c89372c5a64caf9e3
gitleif <leifnesheim@hotmail.com>
2019-05-25 10:46:02 +0200

    Add files via upload

---
+++ 2018/data.php
@@ -0,0 +1,8 @@
+<?php
+// mediaDatafile create by the supersimplemediabrowser
+// Build Folder : G:\websider\github_supersimplemediabrowser/2018/
+// Files Found : 2
+// Build Date : 2019-05-25
+$Data[] = array('File'=>'Travel\image5.jpg','Url'=>'');
+$Data[] = array('File'=>'image4.jpg','Url'=>'');
+?>
---
+++ 2019/data.php
@@ -0,0 +1,9 @@
+<?php
+// mediaDatafile create by the supersimplemediabrowser
+// Build Folder : G:\websider\github_supersimplemediabrowser/2019/
+// Files Found : 3
+// Build Date : 2019-05-25
+$Data[] = array('File'=>'february\image3.jpg','Url'=>'');
+$Data[] = array('File'=>'image1.jpg','Url'=>'');
+$Data[] = array('File'=>'january\image2.jpg','Url'=>'');
+?>
